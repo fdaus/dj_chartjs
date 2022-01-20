6c4a659a4b5309c1bec951fdd5080e5f57445936
fdaus <fdaus.isa@gmail.com>
2022-01-20 12:29:40 +0800

    initial commit

---
+++ chartapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from . models import Product
+from . forms import ProductForm
+# Create your views here.
+
+def index(request):
+    product = Product.objects.all()
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = ProductForm()
+    context = {
+        "products": product,
+        "form": form
+    }
+
+    return render(request, 'chartapp/index.html', context)
